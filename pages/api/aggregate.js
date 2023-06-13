664bb40c70c7fd37439bddd9e7f0be4d1f9eb68d
Clark <thecluskin@gmail.com>
2023-06-13 00:45:50 -0400

    adding console.log to confirm if it is working

--- pages/api/aggregate.js
+++ pages/api/aggregate.js
@@ -1,10 +1,9 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
     // Process a POST request
-    res
-      .status(200)
-      .json({ message: "received" });
-    
+    const body = req.body;
+    console.log({body})
+    res.status(200).json({ body });
   } else {
     // Handle any other HTTP method
     res.status(400).json({ message: "only post is supported" });
