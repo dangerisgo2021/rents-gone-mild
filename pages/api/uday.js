7b0d813be1249554739253363a7529fa4bd3fc65
Clark <thecluskin@gmail.com>
2023-06-20 22:30:31 -0400

    adding css and scss added css reset added layout and homepage content added color variables removed unused jsconfig for alias

---
+++ pages/api/uday.js
@@ -0,0 +1,7 @@
+import { rootAggregator } from "../../src/domains/rootAggregator.js";
+
+export default async function handler(req, res) {
+
+    res.status(200).json({ message: "you the best arouynd ~~~" });
+  
+}
