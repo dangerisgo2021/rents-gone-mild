9ca52eab77fe88f1af86adffd0d73b4195d405a7
Clark <thecluskin@gmail.com>
2023-07-08 12:07:30 -0400

    fix imports

--- src/session/components/session-provider.js
+++ src/session/components/session-provider.js
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { userLoggedInEvent } from "../../user/events/userLoggedInEvent.js";
-import { sendMessage } from "../../messages/services/sendMessage.js";
+import { sendClientMessage } from "../../client/services/sendClientMessage.js";
 import React from "react";
 import { store } from "../../store/store.js";
 import { useStore } from "../../store/hooks/useStore.js";
@@ -18,7 +18,7 @@ export const SessionProvider = ({ children }) => {
         authorization: user.email,
       };
       dispatch(userLoggedInAction({ user }));
-      sendMessage({ message, options }).catch((err) => {
+      sendClientMessage({ message, options }).catch((err) => {
         console.error(err, "sendMessage(userLoggedInEvent({user})) failed");
       });
     }
