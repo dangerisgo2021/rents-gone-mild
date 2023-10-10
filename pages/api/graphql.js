6487eec8927a5a71c8a24c3a710839553a9436dc
Clark <thecluskin@gmail.com>
2023-10-10 00:49:26 -0400

    missed one

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,7 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { resolvers } from "../../src/graphql/resolvers.js";
-import { typeDefs } from "../../src/graphql/typeDefs.js";
+import { typeDefs } from "../../src/graphql/type-defs.js";
 import { getUserFromAuthorizationHeaderService } from "../../src/user/service/get-user-from-authorization-header.js"
 
 const server = new ApolloServer({
