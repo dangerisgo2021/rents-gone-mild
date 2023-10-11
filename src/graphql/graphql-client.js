026ebbfd9baf6b15a43bbdc5e2173ba5a2ee8bc6
Clark <thecluskin@gmail.com>
2023-10-10 20:33:44 -0400

    fixed env var name

--- src/graphql/graphql-client.js
+++ src/graphql/graphql-client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const uri = process.env.GRAPHQL_URI;
+const uri = process.env.NEXT_PUBLIC_GRAPHQL_URI;
 console.log({uri})
 export const graphqlClient = new ApolloClient({
   uri,
